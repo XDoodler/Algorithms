ed8d3776eb6dc6114efc3b445a2ef4b278c56158
Soham Chakrabarti <soham17041998@gmail.com>
2019-04-05 12:31:56 +0530

    Create priority queue.cpp
    Committed Priority Queue

---
+++ priority queue.cpp
@@ -0,0 +1,27 @@
+priority_queue<int, vector<int>, greater<int>> Q;
+//to input elements in a non-increasing order 
+
+priority_queue<int> Q;
+//to input elements in a non-decreasing order
+
+	Q.push(X);
+  //insert element
+  
+  int32_t main()
+  { 
+    for (int i = 0; i < N; i++)
+		{
+			cin >> X;
+			Q.push(X);
+			cout << Q.top() << " ";
+		}
+    
+    return 0;
+  }
+  
+  /*
+  -------------------------------------------------------------------------------------------------------------------------
+  
+  Problems : https://www.codechef.com/problems/TSECJ05
+  Solution : https://www.codechef.com/viewsolution/23672310
+  */
