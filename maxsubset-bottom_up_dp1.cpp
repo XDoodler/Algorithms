9c29ad2a9d5b225c34b3d586cce869529c735658
Soham Chakrabarti <soham17041998@gmail.com>
2019-04-23 12:34:45 +0530

    Update maxsubset-bottom_up_dp1.cpp

--- maxsubset-bottom_up_dp1.cpp
+++ maxsubset-bottom_up_dp1.cpp
@@ -12,10 +12,12 @@ int maxSubsetSum(vector<int> arr) {
 }
 
 /*
+Problem : https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming
+Solution : https://www.hackerrank.com/challenges/max-array-sum/submissions/code/105428447?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming
 
 Given an array of integers, find the subset of non-adjacent elements with the maximum sum. Calculate the sum of that subset.
 
-For example, given an array  we have the following possible subsets:
+For example, given an array arr=[2 , 1, 3 ,-4 , 5]  we have the following possible subsets:
 
 Subset      Sum
 [-2, 3, 5]   6
