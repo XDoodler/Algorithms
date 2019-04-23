142f24eb0c53a0f992c524d3115b6e263c3489e2
Soham Chakrabarti <soham17041998@gmail.com>
2019-04-23 12:49:27 +0530

    Create topological_sort.cpp

---
+++ topological_sort.cpp
@@ -0,0 +1,36 @@
+#define <bits/stdc++.h>
+
+using namepsace std;
+
+int n; // number of vertices
+vector<vector<int>> adj; // adjacency list of graph
+vector<bool> visited;
+vector<int> ans;
+
+void dfs(int v) {
+    visited[v] = true;
+    for (int u : adj[v]) {
+        if (!visited[u])
+            dfs(u);
+    }
+    ans.push_back(v);
+}
+
+
+
+void topological_sort() {
+    visited.assign(n, false);
+    ans.clear();
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i])
+            dfs(i);
+    }
+    reverse(ans.begin(), ans.end());
+}
+
+
+int32_t main()
+{
+//inputs and pass to topological_sort()
+}
+
