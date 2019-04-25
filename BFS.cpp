b46a56500e4a54ba568a0d60c9545a0cba972098
Soham Chakrabarti <soham17041998@gmail.com>
2019-04-25 17:00:43 +0530

    Breadth First Search

---
+++ BFS.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+void breadth_first_search(vector<list<int>> graph,int src){
+    vector<bool>visited(graph.size(),false);
+    queue<int>Q;
+    Q.push(src);
+    visited[src] = true;
+    while(!Q.empty()){
+        int vertex = Q.front(); Q.pop();
+        cout << vertex << " ";
+        for(list<int>::iterator itr = graph[vertex].begin();itr!=graph[vertex].end();itr++){
+            if(!visited[*itr])
+                Q.push(*itr);
+            visited[*itr] = true;
+        }
+    }
+}
+
+
+int32_t main(){
+    vector<list<int>> graph;
+    int v,e,src,des;
+    cin >> v >> e;
+    graph.resize(v);
+    while(e--){
+        cin >> src >> des;
+        graph[src].push_back(des);
+        graph[des].push_back(src);
+    }
+    cin >> src;
+    breadth_first_search(graph,src);
+    return 0;
+}
