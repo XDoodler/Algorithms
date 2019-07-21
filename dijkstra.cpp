821de8fa74194d5bcf8df3b9dd610297b480ea79
Soham Chakrabarti <soham17041998@gmail.com>
2019-07-21 14:49:24 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,76 +1,81 @@
-  
-/* 
-   codechef : nuttela
-   
-   Dijkstra Algorithm 
-*/
-
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
+#define int unsigned long long
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define TC int t; cin >> t; while(t--)
 
-const int N=1e6+6;
 int n,m;
-int arrival[N], departure[N], vis[N], parent[N];
-vector<pair<int, int> > g[N];
+const int MAX = 1e5+5;
+const int inf = 1e18+18;
 
-void dijkstra(int source, int destination)
+vector<pair<int,int>> G[MAX];
+int arrival[MAX],departure[MAX],visited[MAX],path[MAX];
+
+void dijsktra(int source, int dest)
 {
-	for(int i=1;i<=n;i++)
-	{
-		arrival[i]=1e18;
-		departure[i]=1e18;
-		vis[i]=0;
-	}
-	arrival[source]=0;
-	set<pair<int, int> > s;
-	s.insert({0, source});
-	while(!s.empty())
-	{
-		auto x = *(s.begin());
-		s.erase(x);
-		vis[x.second]=1;
-		departure[x.second]=arrival[x.second];
-		for(auto it:g[x.second])
+	set<pair<int,int>> st;
+	arrival[source] = 0;
+	st.insert({0,source});
+	while(!st.empty())
+	{	cout << endl;
+		//for(auto x : st)cout << x.first << ' ' << x.second << ' ';
+		auto x = *(st.begin());
+		st.erase(x);
+		visited[x.second] = 1;
+		departure[x.second] = arrival[x.second];
+		for(auto it:G[x.second])
 		{
 			if(arrival[it.first] > departure[x.second] + it.second)
 			{
-				s.erase({arrival[it.first], it.first});
+				st.erase({arrival[it.first], it.first});
 				arrival[it.first]=departure[x.second] + it.second;
-				s.insert({arrival[it.first], it.first});
-				parent[it.first]=x.second;
+				st.insert({arrival[it.first], it.first});
+				path[it.first]=x.second;
 			}
 		}
 	}
-	if(!vis[destination])
-	{
+		if(!visited[dest])
+		{
 		cout<<"-1";
 		return;
-	}
-	int v=destination;
-	vector<int> ans;
-	while(parent[v])
-	{
-		ans.push_back(v);
-		v=parent[v];
-	}
-	ans.push_back(source);
-	reverse(ans.begin(), ans.end());
-	for(auto it:ans)
+		}
+		int v = dest;
+		vector<int> ans;
+		while(path[v])
+		{
+			//cout << v << ' ';
+			ans.push_back(v);
+			v=path[v];
+		}
+		for (int i = 1; i <=n; ++i)
+		{
+			cout << path[i];
+		}
+		ans.push_back(source);
+		reverse(ans.begin(), ans.end());
+		for(auto it:ans)
 		cout<<it<<" ";
 }
-int32_t main()
-{
+int32_t main() {
+    IOS;
 
-	cin >> n>> m;
-	for(int i=1; i<=m; i++){
-		int u,v,w;
-		cin >> u >> v >>w;
-		g[u].push_back({v,w});
-		g[v].push_back({u,w});
-	}
+    int u,v,w;
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i)
+    {
+    	cin >> u >> v >> w;
+    	G[u].push_back({v,w});
+    	G[v].push_back({u,w});
+    }
+    /*for (int i = 1; i <= n; ++i)
+	{
+		arrival[i] = inf;
+		departure[i] = inf;
+	}*/
+    dijsktra(1,n);
 
-	dijkstra(1,n);
-	
-	return 0;
+    return 0;
 }
+
+//(0,1)
+// (2,2)()
