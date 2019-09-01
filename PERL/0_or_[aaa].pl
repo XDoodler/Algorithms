c0b103b0ef2a19d5cfa0b94647a73f9044a50d9c
Soham Chakrabarti <soham17041998@gmail.com>
2019-09-01 11:43:51 +0530

    Create 0_or_[aaa].pl

---
+++ PERL/0_or_[aaa].pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+#nuttela
+$text1 = "0123";
+$text2 = "aaaaaaaabbbbbbb";
+$text3 = "bcdefgh";
+#$REGEX_GENERATED = /^0|[a]/;
+$fl1=0;$fl2=0;$fl3=0;
+if($text1 =~ /^0|[a]/){
+   $fl1=1;
+}
+if($text2 =~ /^0|[a]/){
+   $fl2=1;
+}
+if($text3 =~ /^0|[a]/){
+   $fl3=1;
+}
+print "$fl1\n$fl2\n$fl3\n";
