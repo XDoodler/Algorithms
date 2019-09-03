ac9e8ad2cfd83cbb193f4db874ddc334f539d063
Soham Chakrabarti <soham17041998@gmail.com>
2019-09-03 10:57:14 +0530

    Update GSS1.cpp

--- SPOJ/GSS1.cpp
+++ SPOJ/GSS1.cpp
@@ -1,12 +1,3 @@
-/*
- Status WA.
- Problem Link : https://www.spoj.com/problems/GSS1/
- 
- What am I missing? HELP NEEDED!
-*/
-
-
-
 #pragma Gpref optimize("O3")
 //#pragma Gpref optimize("Ofast")
 //#pragma Gpref target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
@@ -29,131 +20,61 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 const int MOD = (int)1e9+7;
-const int MAX = 1e5+5;
+const int MAX = 5e4+4;
 typedef pair<int, int> prs;
 
-int SET = 1073741823;
-int n,k,q,rem,cnt=0,ll,rr;
-int T[4*MAX];
-vector<int> arr, input_arr,blocks;
-vector<int> parent,size;    
-int block_size;
-/*
-void pre(int n){
-    parent.resize(n+1,0);size.resize(n+1,0);
-    rep(i,1,n+1){
-        parent[i] = i;
-        size[i] = 1;
-    }
+typedef struct data{
+    int sum, pref, suff, ans;
+}data;
+
+data combine(data l, data r)
+{
+    data d;
+    d.sum = l.sum + r.sum;
+    d.pref = max(l.pref, l.sum + r.pref);
+    d.suff = max(r.suff, r.sum + l.suff);
+    d.ans = max({l.ans,r.ans,l.suff+r.pref});
+    return d;
 }
-int find_parent(int v){
-     if (v == parent[v])
-        return v;
-    return parent[v] = find_parent(parent[v]);
+
+data mkdata(int v){
+    data d;
+    d.sum = d.pref = d.suff = d.ans = v;
+    return d;
 }
-void union_set(int a,int b){
-    a = find_parent(a);
-    b = find_parent(b);
-    if (a != b) {
-        if (size[a] < size[b])
-            swap(a, b);
-        parent[b] = a;
-        size[a] += size[b];
-    }
-}
-*/
-void build(int node,int s,int e){
+
+data T[4*MAX];vector<int> arr;
+
+void build(int node, int s, int e){
     if(s==e){
-        T[node] = arr[s];
+        T[node]=mkdata(arr[s]);
         return;
     }
-    int m=(s+e)>>1;
-    build(node<<1,s,m);
-    build(node<<1|1,m+1,e);
-    int total_sum=T[node<<1]+T[node<<1|1];
-    int max_sum=max(T[node<<1],T[node<<1|1]);
-    T[node]=max(total_sum,max_sum);
-}
-int query(int node,int s,int e,int l,int r){
-    if(e<l || s>r) return 0; //no overlap 
-    if(l<=s && r>=e) return T[node]; //total overlap
-    int mid = (s+e)>>1;
-    return (query(node<<1,s,mid,l,r)+query(node<<1|1,mid+1,e,l,r));
+    int mid=(s+e)>>1;
+    build(node<<1,s,mid);build(node<<1|1,mid+1,e);
+    T[node]=combine(T[node<<1],T[node<<1|1]);
 }
 
-/*
-void upd(int node,int s,int e,int p,int new_val){
-    if(s == e)T[node] = new_val;
-    else
-    {
-        int mid = (s+e)>>1;
-        if(p <= mid)
-        {
-            upd(node<<1, s, mid, p, new_val);
-        }
-        else
-        {
-            upd(node<<1|1, mid+1, e, p, new_val);
-        }
-        T[node] = max(T[node<<1],T[node<<1|1]);
-    }
+data query(int node,int s,int e,int l,int r){
+    if(e<l||s>r)return mkdata(0);
+    if(l>=s&&r>=e)return T[node];
+    int mid=(s+e)>>1;
+    return combine(query(node<<1,s,mid,l,r),query(node<<1|1,mid+1,e,l,r));
 }
 
-void SQRT_update(int index, int val){
-    int blk = index/block_size;
-    blocks[blk] += (val - input_arr[index]);
-    input_arr[index] = val;
-}
-
-int SQRT_query(int l, int r){
-int sum = 0; 
-    while (l<r and l%block_size!=0 && l!=0) 
-    { 
-        sum += input_arr[l]; 
-        l++; 
-    } 
-    while (l+block_size <= r) 
-    { 
-        sum += blocks[l/block_size]; 
-        l += block_size; 
-    } 
-    while (l<=r) 
-    { 
-        sum += input_arr[l]; 
-        l++; 
-    } 
-    return sum;
-}
-
-void build_SQRT_DECOMPED_TREE(int n){
-    block_size = ceil(sqrt(n));
-    blocks.resize(block_size,0);
-    int index_of_block = -1;
-    rep(i,0,n){
-        if(i % block_size==0){
-            index_of_block++; 
-        }
-
-        blocks [index_of_block] += input_arr[i];
-    }
-}
-*/
-int32_t main(){
-    //freopen("ic1.txt", "r", stdin);
-    //freopen("oc1.txt", "w", stdout);
+int32_t main() {
     IOS;
-       
-        int n,q,x,y,z,t;
-        cin >> n;
-        arr.resize(n,0);
-        //input_arr.resize(n,0);
-        rep(i,0,n)cin>>arr[i];
-        build(1,0,n-1);
-        //build_SQRT_DECOMPED_TREE(n);
-        cin >> q;
-        while(q--){
-            cin >> x >> y;
-            cout << query(1,0,n-1,x-1,y-1) <<endl;
-        }
-        return 0;
+    int n,q;
+    scanf("%lld",&n);
+    arr.resize(n,0);
+    rep(i,0,n)scanf("%lld",&arr[i]);
+    build(1,0,n-1);
+    scanf("%lld",&q);
+    while(q--)
+    {
+        int l,r;
+        scanf("%lld%lld",&l,&r);
+        printf("%lld\n",query(1,0,n-1,l-1,r-1).ans);
     }
+    return 0;
+} 
