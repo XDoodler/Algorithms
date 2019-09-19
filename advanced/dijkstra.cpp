2151eabe40d2b8db495a89a6434496c533e893b6
Soham Chakrabarti <soham17041998@gmail.com>
2019-09-19 11:15:46 +0530

    Create dijkstra.cpp

---
+++ advanced/dijkstra.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define ll unsigned long long
+
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+using namespace std;
+
+
+const int MOD = 1e9+7;
+const int MAX = 1e7;
+vector<int> dis;
+vector<bool> processed(MAX, false);
+
+const int inf = INFINITY;
+void dijkstra(int source){
+  priority_queue<pair<int,int>> q;
+  q.push({0,source});
+  dis.resize(MAX,inf);
+  dis[source] = 0;
+
+  while(!q.empty()){
+    int u = q.top().second;
+    q.pop();
+    if(processed[u]) continue;
+        processed[u] = true;
+    for(auto y : G[u]){
+      int v = y.first, w = y.second;
+      if(dis[u] + w < dis[v]){
+        dis[v] = dis[u] + w;
+        q.push(-dis[v],v);
+      }
+    }
+  }
+}
+int main()
+{
+  dijkstra(source);
+  return 0;
+}
