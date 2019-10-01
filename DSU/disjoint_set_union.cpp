82a4e41621f2961505f3177dd5d76187272fb5fe
Arkapravo Ghosh <arkapravoghosh98@gmail.com>
2019-10-01 14:15:35 +0530

    Update disjoint_set_union.cpp

--- DSU/disjoint_set_union.cpp
+++ DSU/disjoint_set_union.cpp
@@ -4,44 +4,38 @@
    Disjoint Set Unions (Union Find and Path Compression
 */
 
-
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
 
 const int N = 1e6+6;
 
-int link[N], size[N];
+int parent[N], size[N];
 
 //find the parent
 int find(int x) {
-while (x != link[x]) x = link[x];
-return x;
+    return x == parent[x] ? x : parent[x] = find(parent[x]);
 }
 
 //unite , merge and change parent
 void unite(int a, int b) {
-a = find(a);
-b = find(b);
-if (size[a] < size[b]) swap(a,b);
-size[a] += size[b];
-link[b] = a;
+    a = find(a);
+    b = find(b);
+    if(a == b) return;
+    if(size[a] < size[b]) swap(a,b);
+    size[a] += size[b];
+    parent[b] = a;
 }
 
-int32_t main()
-{
-	
-	for (int i = 1; i <= 5; ++i)
-	{
-		link[i] = i;
-		size[i] = 1;
-	}
+int32_t main() {
+    for (int i = 1; i <= 5; ++i) {
+	parent[i] = i;
+	size[i] = 1;
+    }
 
-
-	//process DSUs over the array and compute
-	//unite(1,2); 
-	//unite(1,5);
-	//unite(5,4);
-	return 0;
+    //process DSUs over the array and compute
+    //unite(1,2); 
+    //unite(1,5);
+    //unite(5,4);
+    return 0;
 }
-
