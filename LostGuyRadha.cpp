4991ba191af8a833d65a977ef16adc77d68bb67c
Soham Chakrabarti <soham17041998@gmail.com>
2019-12-27 20:00:50 +0530

    Create LostGuyRadha.cpp

---
+++ LostGuyRadha.cpp
@@ -0,0 +1,40 @@
+/* nuttela - Soham Chakrabarti */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+
+typedef long long ll;
+typedef long double ld;
+typedef pair <int, int> pii;
+
+const int N = 1e3;
+const int inf = -1;
+const int MOD = 1e9 + 7;
+
+int main() {
+  ll tt; cin >> tt;
+  while(tt--) {
+    int n, q;
+    cin >> n >> q;
+    int f[n];
+    set <int> a;
+    for (int i = 0; i < n; ++i)
+    {
+      int x;
+      cin >> x;
+      a.insert(x);
+      f[i] = *--a.end(); //last element of set
+    }
+    for (int i = 0; i < q; ++i)
+    {
+      int x;
+      cin >> x; x;
+      cout << f[x - 1] << endl;
+    }
+  }
+  return 0;
+}
