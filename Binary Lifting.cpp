8e3231f1d655af280a78a4cfe234d420e721f3bb
Soham Chakrabarti <soham17041998@gmail.com>
2020-03-31 13:25:35 +0530

    Update and rename Binary_Lifting.cpp to Binary Lifting.cpp

--- Binary Lifting.cpp
+++ Binary Lifting.cpp
@@ -1,4 +1,11 @@
+/*
 
+Binary Lifting is an algorithm that is used in finding the Lowest Common ancestor of two nodes in a tree.
+Below is the clean implementation of the algorithm.
+
+For ref : https://www.geeksforgeeks.org/lca-in-a-tree-using-binary-lifting-technique/
+
+*/
 int n,m,q,mx;
 const int MAX = 1e5+5;
 typedef pair<int, int> pairs;
