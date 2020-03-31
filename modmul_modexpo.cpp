0ba6f10e3cc7ece801ceeffbf00aa63d97bd396c
Soham Chakrabarti <soham17041998@gmail.com>
2020-03-31 12:45:55 +0530

    Update and rename modmul.cpp to modmul_modexpo.cpp

--- modmul_modexpo.cpp
+++ modmul_modexpo.cpp
@@ -1,17 +1,4 @@
-//Clash Credenz 2019 Wildcard
-//Team : Runtime Terror
 
-#include <bits/stdc++.h>
-
-#define ll unsigned long long
-
-#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-using namespace std;
-
-
-const int MOD = 1e9+7;
-const int MAX = 1e7;
 ll modmul(ll a,ll b){
   ll res = 0;
   a = a % MOD;
