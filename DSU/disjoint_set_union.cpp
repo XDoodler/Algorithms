252fb03f20c787132bbc889806ba2e288b23bf9b
Soham Chakrabarti <soham17041998@gmail.com>
2020-03-31 12:42:44 +0530

    Update disjoint_set_union.cpp

--- DSU/disjoint_set_union.cpp
+++ DSU/disjoint_set_union.cpp
@@ -1,41 +1,25 @@
-/* 
-   codechef : nuttela
-   
-   Disjoint Set Unions (Union Find and Path Compression
-*/
-
-#include <bits/stdc++.h>
-#define int long long
-using namespace std;
-
-const int N = 1e6+6;
 
 int parent[N], size[N];
 
-//find the parent
-int find(int x) {
-    return x == parent[x] ? x : parent[x] = find(parent[x]);
+int find(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find(parent[v]);
 }
 
-//unite , merge and change parent
+void make(int v) {
+    parent[v] = v;
+    size[v] = 1;
+}
+
 void unite(int a, int b) {
     a = find(a);
     b = find(b);
-    if(a == b) return;
-    if(size[a] < size[b]) swap(a,b);
-    size[a] += size[b];
-    parent[b] = a;
-}
-
-int32_t main() {
-    for (int i = 1; i <= 5; ++i) {
-	parent[i] = i;
-	size[i] = 1;
+    if (a != b) {
+        if (size[a] < size[b])
+            swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
     }
-
-    //process DSUs over the array and compute
-    //unite(1,2); 
-    //unite(1,5);
-    //unite(5,4);
-    return 0;
 }
+
