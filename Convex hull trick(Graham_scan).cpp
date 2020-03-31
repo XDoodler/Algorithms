32df7800630d3cfddcfb2b4923f4b6dd9bd90d13
Soham Chakrabarti <soham17041998@gmail.com>
2020-03-31 13:43:51 +0530

    Update and rename convex_hull_trick(graham_scan).cpp to Convex hull trick(Graham_scan).cpp

--- Convex hull trick(Graham_scan).cpp
+++ Convex hull trick(Graham_scan).cpp
@@ -1,23 +1,4 @@
-//Convex Hull Trick.
 
-
-#pragma GCC optimize("O3")
-//~ #pragma GCC optimize("Ofast")
-//~ #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
-//~ #pragma GCC optimize("unroll-loops")
-#include <bits/stdc++.h>
-#define ll long long
-#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define TC int t; cin >> t; while(t--)
-#define rep(i,j,k) for(int i=j; i<k; i++)
-#define endl '\n'
-#define pb push_back
-const int MAX = (int)1e5+5;
-using namespace std;
-
-
-vector<int> G[MAX], euler_tour;
- 
 struct point
 {
     int x, y;
