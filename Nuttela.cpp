29416ab550cdd94de4a63ae4f0cea399de8a96cf
Soham Chakrabarti <soham17041998@gmail.com>
2020-05-19 11:44:45 +0530

    Create Nuttela.cpp

---
+++ Nuttela.cpp
@@ -0,0 +1,38 @@
+/* nuttela - Soham Chakrabarti */
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define all(v) v.begin(),v.end()
+#define pb push_back
+#define ins insert
+#define rep(i,j,k) for(ll i=j;i<k;i++)
+#define per(i,j,k) for(ll i=j;i>=k;--i)
+#define in freopen("input.txt","r",stdin);
+#define out freopen("output.txt","w",stdout);
+#define ff first
+#define ss second
+ 
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair <ll,ll> pll;
+ 
+const int N = 1e5 + 5;
+const int M = 1e5 + 5;
+const int inf = INT_MAX;
+const int MOD = 1e9 + 7;
+
+int32_t main() {
+    io;
+    // in;out;
+    int t;
+    cin>>t;
+    while(t--) {
+    	//code goes here
+
+    }
+    return 0;
+}
