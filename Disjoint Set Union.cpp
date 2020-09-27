7e8ca0926e020dfa9961b282fa82ac6867104f3e
Soham Chakrabarti <soham17041998@gmail.com>
2020-09-27 23:37:53 +0530

    Update Disjoint Set Union.cpp

--- Disjoint Set Union.cpp
+++ Disjoint Set Union.cpp
@@ -23,3 +23,28 @@ void unite(int a, int b) {
     }
 }
 
+
+class dsu {
+ public:
+  vector<int> p;
+  int n;
+ 
+  dsu(int _n) : n(_n) {
+    p.resize(n);
+    iota(p.begin(), p.end(), 0);
+  }
+ 
+  inline int get(int x) {
+    return (x == p[x] ? x : (p[x] = get(p[x])));
+  }
+ 
+  inline bool unite(int x, int y) {
+    x = get(x);
+    y = get(y);
+    if (x != y) {
+      p[x] = y;
+      return true;
+    }
+    return false;
+  }
+};
